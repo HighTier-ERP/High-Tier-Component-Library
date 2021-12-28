94fb54d8e265f53b0c9140151b4f95d181152f0a
Jack Wuerfel <jwuerfel23@gmail.com>
2021-12-28 18:52:51 -0500

    update htcf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import "./lib/styles/high-tier.scss";
+import "./lib/styles/high-tier-css.scss";
 
 import { Accordion } from "./lib";
 
 function App() {
   return (
-    <div className="flex flew-row pt-30px">
-      <header className="column-3 flex flex-row items-center content-center">
+    <div className="flex flew-row">
+      <aside className="column-3 flex flex-row items-center content-center b-r-default p-30px h-100vh">
         <img
-          className="w-65px mr-20px"
+          className="w-65px mr-10px"
           src="https://uskhpjjjepcdouiydafe.supabase.in/storage/v1/object/sign/high-tier/high-tier_icon.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJoaWdoLXRpZXIvaGlnaC10aWVyX2ljb24uc3ZnIiwiaWF0IjoxNjQwNzExNDk4LCJleHAiOjE5NTYwNzE0OTh9.T39bhQl9VrWFXYcX9Cd4my0LxFWNpu2CY9hjMWy526E"
           alt="High Tier Component Library Logo and WordMark"
         />
         <span className="flex flex-column italic">
-          <h1 className="f-s-25px text-uppercase l-h-1_2em">High Tier </h1>
-          <h2 className="f-s-16px f-w-500"> Component Library</h2>
+          <h1 className="f-s-20px text-uppercase f-w-700 l-h-1_2em l-s-3_5px">
+            High Tier{" "}
+          </h1>
+          <h2 className="f-s-12px f-w-500 text-uppercase l-s-1px">
+            {" "}
+            Component Library
+          </h2>
         </span>
-      </header>
-      <div className="column-9">
-        <Accordion />
+      </aside>
+      <div className="column-9 bg-gray-2">
+        <Accordion accordionType="single" />
       </div>
     </div>
   );
