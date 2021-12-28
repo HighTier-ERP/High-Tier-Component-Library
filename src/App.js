755823602e6ce65ab7b7556e940a85f0f4155507
Jack Wuerfel <jwuerfel23@gmail.com>
2021-12-28 12:39:30 -0500

    updated library with scss base

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { Accordion } from "./lib";
 function App() {
   return (
     <div className="flex flew-row pt-30px">
-      <header className="column-4 flex flex-row items-center content-center">
+      <header className="column-3 flex flex-row items-center content-center">
         <img
-          className="w-75px mr-20px"
+          className="w-65px mr-20px"
           src="https://uskhpjjjepcdouiydafe.supabase.in/storage/v1/object/sign/high-tier/high-tier_icon.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJoaWdoLXRpZXIvaGlnaC10aWVyX2ljb24uc3ZnIiwiaWF0IjoxNjQwNzExNDk4LCJleHAiOjE5NTYwNzE0OTh9.T39bhQl9VrWFXYcX9Cd4my0LxFWNpu2CY9hjMWy526E"
+          alt="High Tier Component Library Logo and WordMark"
         />
-        <h1 className="f-s-25px">High Tier Component Library</h1>
+        <span className="flex flex-column italic">
+          <h1 className="f-s-25px text-uppercase l-h-1_2em">High Tier </h1>
+          <h2 className="f-s-16px f-w-500"> Component Library</h2>
+        </span>
       </header>
-      <div className="column-8">
+      <div className="column-9">
         <Accordion />
       </div>
     </div>
