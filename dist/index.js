e31f8a1e6dd71fca0e14337c73e045a685afa21b
Jack Wuerfel <jwuerfel23@gmail.com>
2022-01-04 16:55:23 -0500

    added icons to library

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,172 @@ Object.defineProperty(exports, "Accordion", {
     return _Accordion.default;
   }
 });
+Object.defineProperty(exports, "ArrowCircleIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ArrowCircleIcon;
+  }
+});
+Object.defineProperty(exports, "Calculator", {
+  enumerable: true,
+  get: function get() {
+    return _icons.Calculator;
+  }
+});
 Object.defineProperty(exports, "Calendar", {
   enumerable: true,
   get: function get() {
     return _Calendar.default;
+  }
+});
+Object.defineProperty(exports, "CarIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CarIcon;
+  }
+});
+Object.defineProperty(exports, "ChatIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ChatIcon;
+  }
+});
+Object.defineProperty(exports, "CheckIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CheckIcon;
+  }
+});
+Object.defineProperty(exports, "CirclePlusIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CirclePlusIcon;
+  }
+});
+Object.defineProperty(exports, "ClockIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ClockIcon;
+  }
+});
+Object.defineProperty(exports, "CloseIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CloseIcon;
+  }
+});
+Object.defineProperty(exports, "CloudIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CloudIcon;
+  }
+});
+Object.defineProperty(exports, "CogIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CogIcon;
+  }
+});
+Object.defineProperty(exports, "CompanyIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CompanyIcon;
+  }
+});
+Object.defineProperty(exports, "CreditCardIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.CreditCardIcon;
+  }
+});
+Object.defineProperty(exports, "DocumentPlusIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.DocumentPlusIcon;
+  }
+});
+Object.defineProperty(exports, "DocumentSearchIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.DocumentSearchIcon;
+  }
+});
+Object.defineProperty(exports, "DocumentTextIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.DocumentTextIcon;
+  }
+});
+Object.defineProperty(exports, "DotsCircleIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.DotsCircleIcon;
+  }
+});
+Object.defineProperty(exports, "DownIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.DownIcon;
+  }
+});
+Object.defineProperty(exports, "DynamicIcon", {
+  enumerable: true,
+  get: function get() {
+    return _DynamicIcon.default;
+  }
+});
+Object.defineProperty(exports, "EmailIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.EmailIcon;
+  }
+});
+Object.defineProperty(exports, "ExclamationIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ExclamationIcon;
+  }
+});
+Object.defineProperty(exports, "EyeIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.EyeIcon;
+  }
+});
+Object.defineProperty(exports, "FacebookIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.FacebookIcon;
+  }
+});
+Object.defineProperty(exports, "GiftIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.GiftIcon;
+  }
+});
+Object.defineProperty(exports, "GlobeIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.GlobeIcon;
+  }
+});
+Object.defineProperty(exports, "GridIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.GridIcon;
+  }
+});
+Object.defineProperty(exports, "HomeIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.HomeIcon;
+  }
+});
+Object.defineProperty(exports, "InboxIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.InboxIcon;
   }
 });
 Object.defineProperty(exports, "Input", {
@@ -21,10 +183,52 @@ Object.defineProperty(exports, "Input", {
     return _Input.default;
   }
 });
+Object.defineProperty(exports, "KeyIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.KeyIcon;
+  }
+});
+Object.defineProperty(exports, "LeftIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.LeftIcon;
+  }
+});
 Object.defineProperty(exports, "Loading", {
   enumerable: true,
   get: function get() {
     return _Loading.default;
+  }
+});
+Object.defineProperty(exports, "LogoutIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.LogoutIcon;
+  }
+});
+Object.defineProperty(exports, "MailOpenIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.MailOpenIcon;
+  }
+});
+Object.defineProperty(exports, "MenuIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.MenuIcon;
+  }
+});
+Object.defineProperty(exports, "MobileDeviceIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.MobileDeviceIcon;
+  }
+});
+Object.defineProperty(exports, "MoonIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.MoonIcon;
   }
 });
 Object.defineProperty(exports, "MotionDiv", {
@@ -37,6 +241,12 @@ Object.defineProperty(exports, "NotInProduction", {
   enumerable: true,
   get: function get() {
     return _Environments.NotInProduction;
+  }
+});
+Object.defineProperty(exports, "OfficeIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.OfficeIcon;
   }
 });
 Object.defineProperty(exports, "OnlyInLocal", {
@@ -57,16 +267,142 @@ Object.defineProperty(exports, "OnlyInStaging", {
     return _Environments.OnlyInStaging;
   }
 });
+Object.defineProperty(exports, "PrintIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.PrintIcon;
+  }
+});
+Object.defineProperty(exports, "RemoteIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.RemoteIcon;
+  }
+});
+Object.defineProperty(exports, "ReportIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ReportIcon;
+  }
+});
+Object.defineProperty(exports, "ReturnIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ReturnIcon;
+  }
+});
+Object.defineProperty(exports, "RightIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.RightIcon;
+  }
+});
+Object.defineProperty(exports, "SadIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.SadIcon;
+  }
+});
+Object.defineProperty(exports, "SaveIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.SaveIcon;
+  }
+});
+Object.defineProperty(exports, "ScaleIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ScaleIcon;
+  }
+});
+Object.defineProperty(exports, "SearchIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.SearchIcon;
+  }
+});
+Object.defineProperty(exports, "ServerIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ServerIcon;
+  }
+});
+Object.defineProperty(exports, "ShoppingCartIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.ShoppingCartIcon;
+  }
+});
 Object.defineProperty(exports, "SiteLogo", {
   enumerable: true,
   get: function get() {
     return _SiteLogo.default;
   }
 });
+Object.defineProperty(exports, "SparklesIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.SparklesIcon;
+  }
+});
+Object.defineProperty(exports, "StarIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.StarIcon;
+  }
+});
+Object.defineProperty(exports, "SunIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.SunIcon;
+  }
+});
+Object.defineProperty(exports, "TemplateIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.TemplateIcon;
+  }
+});
 Object.defineProperty(exports, "TextArea", {
   enumerable: true,
   get: function get() {
     return _TextArea.default;
+  }
+});
+Object.defineProperty(exports, "TrashIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.TrashIcon;
+  }
+});
+Object.defineProperty(exports, "UpIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.UpIcon;
+  }
+});
+Object.defineProperty(exports, "UsCurrencyIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.UsCurrencyIcon;
+  }
+});
+Object.defineProperty(exports, "UserAddIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.UserAddIcon;
+  }
+});
+Object.defineProperty(exports, "UserGroupIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.UserGroupIcon;
+  }
+});
+Object.defineProperty(exports, "UserIcon", {
+  enumerable: true,
+  get: function get() {
+    return _icons.UserIcon;
   }
 });
 Object.defineProperty(exports, "useIsLocal", {
@@ -98,6 +434,8 @@ var _Accordion = _interopRequireDefault(require("./components/accordion-componen
 
 var _Calendar = _interopRequireDefault(require("./components/calendar-component/Calendar"));
 
+var _DynamicIcon = _interopRequireDefault(require("./components/dynamic-icon-component/DynamicIcon"));
+
 var _Loading = _interopRequireDefault(require("./components/loading-component/Loading"));
 
 var _Input = _interopRequireDefault(require("./components/input-component/Input"));
@@ -109,5 +447,7 @@ var _SiteLogo = _interopRequireDefault(require("./components/site-logo-component
 var _TextArea = _interopRequireDefault(require("./components/text-area-component/TextArea"));
 
 var _Environments = require("./components/environment-component/Environments");
+
+var _icons = require("./icons");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/lib/index.js
+++ src/lib/index.js
@@ -5,13 +5,23 @@
 
 import Accordion from './components/accordion-component/Accordion';
 import Calendar from './components/calendar-component/Calendar';
+import DynamicIcon from './components/dynamic-icon-component/DynamicIcon';
 import Loading from './components/loading-component/Loading';
 import Input from './components/input-component/Input';
 import MotionDiv from './components/motion-div-component/MotionDiv';
 import SiteLogo from './components/site-logo-component/SiteLogo';
 import TextArea from './components/text-area-component/TextArea';
 
-export { Accordion, Calendar, Input, Loading, MotionDiv, SiteLogo, TextArea };
+export {
+	Accordion,
+	Calendar,
+	DynamicIcon,
+	Input,
+	Loading,
+	MotionDiv,
+	SiteLogo,
+	TextArea
+};
 
 // Flags - Hooks and Components
 /*----------------------------------------------------------------------------*/
@@ -27,3 +37,61 @@ export {
 	useIsStaging,
 	useIsLocal
 } from './components/environment-component/Environments';
+
+export {
+	ArrowCircleIcon,
+	Calculator,
+	ChatIcon,
+	CheckIcon,
+	CarIcon,
+	ClockIcon,
+	CloseIcon,
+	CloudIcon,
+	CogIcon,
+	CompanyIcon,
+	CirclePlusIcon,
+	CreditCardIcon,
+	DocumentPlusIcon,
+	DocumentSearchIcon,
+	DocumentTextIcon,
+	DotsCircleIcon,
+	DownIcon,
+	EmailIcon,
+	EyeIcon,
+	PrintIcon,
+	ReturnIcon,
+	ExclamationIcon,
+	GlobeIcon,
+	GridIcon,
+	KeyIcon,
+	HomeIcon,
+	FacebookIcon,
+	GiftIcon,
+	InboxIcon,
+	LeftIcon,
+	LogoutIcon,
+	MailOpenIcon,
+	MobileDeviceIcon,
+	MenuIcon,
+	MoonIcon,
+	OfficeIcon,
+	RightIcon,
+	RemoteIcon,
+	ReportIcon,
+	ScaleIcon,
+	SadIcon,
+	SearchIcon,
+	ServerIcon,
+	ShoppingCartIcon,
+	SparklesIcon,
+	StarIcon,
+	SaveIcon,
+	SunIcon,
+	UserAddIcon,
+	UpIcon,
+	UsCurrencyIcon,
+	UserIcon,
+	UserGroupIcon,
+	TemplateIcon,
+	TrashIcon
+} from './icons';
